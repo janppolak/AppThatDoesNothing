3b2211e806be192f33b15d388a1a76a495acb9c6
Jan Polak <jan.p.polak@gmail.com>
2023-08-28 18:34:30 +0200

    Just deleted commented lines

--- ConsoleApp5/Repository.cs
+++ ConsoleApp5/Repository.cs
@@ -65,38 +65,5 @@ namespace ConsoleApp5
                 connection.Update<T>(entityToBeDeleted);
             }
         }
-
-        //public class FakeRepository : IRepository
-        //{
-        //    public void Delete<T>(T entity) where T : Entity
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-
-        //    public T Get<T>(long id) where T : Entity
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-
-        //    public IEnumerable<T> GetAll<T>() where T : Entity
-        //    {
-        //        throw new System.NotImplementedException();
-        //    }
-
-        //    public void Insert<T>(T entity) where T : Entity
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-
-        //    public void InsertMany<T>(IEnumerable<T> entities) where T : Entity
-        //    {
-        //        throw new System.NotImplementedException();
-        //    }
-
-        //    public void Update<T>(T entityToBeUpdated, Action<T> updateFunc) where T : Entity
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //}
     }
 }
